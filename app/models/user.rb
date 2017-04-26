7c3ecdfe495c55df4610205e162ac3742170e457
sitarovad <danka.sitarova@gmail.com>
2017-04-26 17:55:57 +0200

    Start with creating users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :user_films
+
+  validates :name, presence:true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :login, presence:true, length: { maximum: 200 }, format: { with:VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
