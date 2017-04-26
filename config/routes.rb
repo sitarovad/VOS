ad1eec855b54da689b4f6070b0295d83b9dd2905
sitarovad <danka.sitarova@gmail.com>
2017-04-26 22:53:27 +0200

    Finish sign up page, insert in database, finished 7th chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   get '/help', to: 'static_pages#help', as: 'helf'
   get '/about', to: 'static_pages#about'
+
   root 'static_pages#home'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
