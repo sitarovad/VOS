b16255636a31157fd1d56a4ce755d811f4b65cdb
sitarovad <danka.sitarova@gmail.com>
2017-04-26 21:02:49 +0200

    Change User model, finished 6th chapter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
 
   validates :name, presence:true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :login, presence:true, length: { maximum: 200 }, format: { with:VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :login, presence:true, length: { maximum: 200 },
+            format: { with:VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327104108) do
+ActiveRecord::Schema.define(version: 20170426183556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,10 +81,11 @@ ActiveRecord::Schema.define(version: 20170327104108) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.text "login"
-    t.text "password"
-    t.date "signed_in_at"
+    t.text     "name"
+    t.text     "login"
+    t.text     "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["login"], name: "index_users_on_login", unique: true, using: :btree
   end
 
