fc57601499a44cffdc9991701a63e35897c53489
sitarovad <danka.sitarova@gmail.com>
2017-04-27 12:27:50 +0200

    Basic log in, log out, finished 8th chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
 
   get '/help', to: 'static_pages#help', as: 'helf'
   get '/about', to: 'static_pages#about'
